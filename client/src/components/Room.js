5e4bdcd71e854ddef0162501e8b47fd731177832
Eliwoodno <pbarraux@gmail.com>
2019-02-21 23:26:39 +0100

    room.css

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -3,9 +3,48 @@ import '../styles/Room.css'
 
 const Room = () => {
   return (
-    <div className="Room">
-      <h1>Chatroom!</h1>
-    </div>
+     <div className="Room">
+       <h1>LE BIG GAY</h1>{/* Titre à dynamiser */}
+       <div className="main_container">
+         <div className="chat_display">
+           <div className="messages_container">
+
+             <div className="message">
+               <div className="message_info"><span className="user">{/* User */}Username</span><span className="post_date">{/* Date */} - 18:12  </span></div> 
+               <div className="message_bubble">{/* Content */}Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, modi laborum! Eaque numquam, animi, laboriosam tempore quisquam maxime facilis velit deserunt, aut dolorum quos obcaecati aliquam minus commodi debitis? Porro?</div>
+             </div>
+             {/* <DUPLICATS> */}
+             <div className="message">
+               <div className="message_info"><span className="user">Username</span><span className="post_date"> - 18:12 </span></div> 
+               <div className="message_bubble">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+             </div>
+             <div className="message">
+               <div className="message_info"><span className="user">Username</span><span className="post_date"> - 18:12 </span></div> 
+               <div className="message_bubble">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, modi laborum!</div>
+             </div>
+             <div className="message">
+               <div className="message_info"><span className="user">Username</span><span className="post_date"> - 18:12 </span></div> 
+               <div className="message_bubble">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora omnis velit adipisci nemo inventore est modi unde reprehenderit nisi, harum at quidem ipsam mollitia quibusdam nobis ipsum ratione iure ad?</div>
+             </div>
+             <div className="message">
+               <div className="message_info"><span className="user">Username</span><span className="post_date"> - 18:12 </span></div> 
+               <div className="message_bubble">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sint aliquam accusamus fugit, perspiciatis consequatur beatae nemo? Tempore voluptas illo voluptates? Quo perspiciatis nisi laudantium iure eius ullam consectetur similique.</div>
+             </div>
+             <div className="message">
+               <div className="message_info"><span className="user">Username</span><span className="post_date"> - 18:12 </span></div> 
+               <div className="message_bubble">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nobis minima facilis odio, quasi praesentium ipsa veritatis voluptatem esse, eius molestiae repellendus labore iusto obcaecati sed quidem in. Tempora, facilis.</div>
+             </div>
+             <div className="message">
+               <div className="message_info"><span className="user">Username</span><span className="post_date"> - 18:12 </span></div> 
+               <div className="message_bubble">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, modi laborum! Eaque numquam, animi, laboriosam tempore quisquam maxime facilis velit deserunt, aut dolorum quos obcaecati aliquam minus commodi debitis? Porro?</div>
+             </div>
+             {/* <DUPLICATS/>*/}
+             
+           </div>
+         </div>
+         <div className="user_input">là dedans faut mettre le form pour envoyer des messages</div>
+       </div>
+     </div>
   )
 }
 
--- models/message.model.js
+++ models/message.model.js
@@ -1,19 +1,48 @@
 /*
 Imports & configs
 */
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config()
+//
+
 
 /*
 Model definition
 */
 const messageSchema = new Schema({
-    username: String,
+    author: String,
     content: String,
-    date: Number
+    timestamp: { 
+        
+        type: Date,
+        default: Date.now
+    }, 
+
 })
+
+/*
+Method
+*/
+messageSchema.methods.generateJwt = function generateJwt() {
+    // set expiration
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + 59);
+
+    // JWT creation
+    return jwt.sign({
+        _id: this._id,
+        username: this.author,
+        content: this.content,
+        date: this.timestamp,
+        expireIn: '10s',
+        exp: parseInt(expiry.getTime() / 100, 10)
+    }, process.env.JWT_SECRET)
+};
+
 /*
 Export
 */
-const MessageModel = mongoose.model('message', messageSchema)
-module.exports = MessageModel
+const messageModel = mongoose.model('message', messageSchema);
+module.exports = messageModel;
